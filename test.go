70eadac6058ce0e6cf2de9c2887a9eb811573873
dawnmn <dawnmn@126.com>
2024-03-28 10:53:36 +0800

    提交测试

--- test.go
+++ test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go-com/config"
 	"go-com/core/logr"
 	"go-com/core/my"
@@ -13,7 +12,4 @@ func main() {
 	logr.InitLog("test")
 	app.Mysql = my.NewDb(my.Config{Mysql: config.C.Mysql})
 
-	var m map[string]interface{}
-	app.Mysql.Table("auth_user").Take(&m)
-	fmt.Println(m)
 }
